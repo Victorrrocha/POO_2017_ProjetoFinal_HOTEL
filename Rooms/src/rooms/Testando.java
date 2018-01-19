c8061e5f3cd7193313741570694f5ff27528176f
Pedro Aleph <pedro.aleph.allen@gmail.com>
2018-01-18 20:31:52 -0400

    Quartos

---
+++ Rooms/src/rooms/Testando.java
@@ -0,0 +1,10 @@
+package rooms;
+
+class Testando {
+	public static void main(String[] args) {
+		Suite nova = new Suite();
+		Simples novo = new Simples();
+		novo.ocupando("Cicrano", 05, 2);
+		//novo.ocupando("beltrano", 05, 3);
+	}
+}
